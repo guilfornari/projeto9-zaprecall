bf881f8e4aa5d583a98fc6181641661b76b83b3e
Guilfornari <guilfornari@gmail.com>
2023-03-04 08:25:12 -0300

    created the components and installed the styled components

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import AppBody from "./AppBody";
+
+function App() {
+  return (
+    <div>
+      <AppBody />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/AppBody.js
@@ -0,0 +1,14 @@
+import Bottom from "./Bottom";
+import Deck from "./Deck";
+import Header from "./Header";
+
+export default function AppBody() {
+    return (
+        <div>
+            <Header />
+            <Deck />
+            <Bottom />
+        </div>
+    );
+}
+
